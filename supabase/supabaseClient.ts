f404a8e2ac51ccce60921366f23a5c8aeda982e5
Blemmingan <jyulan@itba.edu.ar>
2023-09-23 13:04:26 -0300

    Fixed broken import

--- supabase/supabaseClient.ts
+++ supabase/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '../types/supabase';
+import { Database } from './types/supabase';
 
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey: string = process.env
