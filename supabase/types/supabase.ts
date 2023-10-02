2d94938fc9daec53c37a4ea9640962d2ddf8037f
Franco Morroni <fmorroni99@gmail.com>
2023-10-02 00:25:22 -0300

    <chore>: Regenerate supabase types with private profile tables

--- supabase/types/supabase.ts
+++ supabase/types/supabase.ts
@@ -4,244 +4,262 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       animals: {
         Row: {
-          age: number | null;
-          back_length: number;
-          breed: string | null;
-          health_rating: number;
-          height: number;
-          id: string;
-          inserted_at: string;
-          name: string;
-          org_id: string;
-          rescue_date: string | null;
-          sex: boolean;
-          species_id: number;
-          temporary_home_id: string | null;
-          temporary_home_until: string | null;
-          updated_at: string;
-          vaccinated: boolean | null;
-          weight: number;
-        };
+          age: number | null
+          back_length: number
+          breed: string | null
+          health_rating: number
+          height: number
+          id: string
+          inserted_at: string
+          name: string
+          org_id: string
+          rescue_date: string | null
+          sex: boolean
+          species_id: number
+          temporary_home_id: string | null
+          temporary_home_until: string | null
+          updated_at: string
+          vaccinated: boolean | null
+          weight: number
+        }
         Insert: {
-          age?: number | null;
-          back_length: number;
-          breed?: string | null;
-          health_rating?: number;
-          height: number;
-          id?: string;
-          inserted_at?: string;
-          name: string;
-          org_id: string;
-          rescue_date?: string | null;
-          sex: boolean;
-          species_id: number;
-          temporary_home_id?: string | null;
-          temporary_home_until?: string | null;
-          updated_at?: string;
-          vaccinated?: boolean | null;
-          weight: number;
-        };
+          age?: number | null
+          back_length: number
+          breed?: string | null
+          health_rating?: number
+          height: number
+          id?: string
+          inserted_at?: string
+          name: string
+          org_id: string
+          rescue_date?: string | null
+          sex: boolean
+          species_id: number
+          temporary_home_id?: string | null
+          temporary_home_until?: string | null
+          updated_at?: string
+          vaccinated?: boolean | null
+          weight: number
+        }
         Update: {
-          age?: number | null;
-          back_length?: number;
-          breed?: string | null;
-          health_rating?: number;
-          height?: number;
-          id?: string;
-          inserted_at?: string;
-          name?: string;
-          org_id?: string;
-          rescue_date?: string | null;
-          sex?: boolean;
-          species_id?: number;
-          temporary_home_id?: string | null;
-          temporary_home_until?: string | null;
-          updated_at?: string;
-          vaccinated?: boolean | null;
-          weight?: number;
-        };
+          age?: number | null
+          back_length?: number
+          breed?: string | null
+          health_rating?: number
+          height?: number
+          id?: string
+          inserted_at?: string
+          name?: string
+          org_id?: string
+          rescue_date?: string | null
+          sex?: boolean
+          species_id?: number
+          temporary_home_id?: string | null
+          temporary_home_until?: string | null
+          updated_at?: string
+          vaccinated?: boolean | null
+          weight?: number
+        }
         Relationships: [
           {
-            foreignKeyName: 'animals_org_id_fkey';
-            columns: ['org_id'];
-            referencedRelation: 'organizations';
-            referencedColumns: ['id'];
+            foreignKeyName: "animals_org_id_fkey"
+            columns: ["org_id"]
+            referencedRelation: "organizations"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: 'animals_species_id_fkey';
-            columns: ['species_id'];
-            referencedRelation: 'species';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
-      asdf: {
-        Row: {
-          created_at: string;
-          date: string | null;
-          health_rating: number;
-          height: number;
-          id: string;
-          sex: boolean;
-          sp_id: number;
-          temporary_home_until: string | null;
-        };
-        Insert: {
-          created_at?: string;
-          date?: string | null;
-          health_rating?: number;
-          height: number;
-          id?: string;
-          sex: boolean;
-          sp_id: number;
-          temporary_home_until?: string | null;
-        };
-        Update: {
-          created_at?: string;
-          date?: string | null;
-          health_rating?: number;
-          height?: number;
-          id?: string;
-          sex?: boolean;
-          sp_id?: number;
-          temporary_home_until?: string | null;
-        };
-        Relationships: [
-          {
-            foreignKeyName: 'asdf_sp_id_fkey';
-            columns: ['sp_id'];
-            referencedRelation: 'species';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
+            foreignKeyName: "animals_species_id_fkey"
+            columns: ["species_id"]
+            referencedRelation: "species"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       organizations: {
         Row: {
-          id: string;
-          name: string;
-          reputation: number | null;
-        };
+          id: string
+          name: string
+          reputation: number | null
+        }
         Insert: {
-          id: string;
-          name: string;
-          reputation?: number | null;
-        };
+          id: string
+          name: string
+          reputation?: number | null
+        }
         Update: {
-          id?: string;
-          name?: string;
-          reputation?: number | null;
-        };
+          id?: string
+          name?: string
+          reputation?: number | null
+        }
         Relationships: [
           {
-            foreignKeyName: 'organizations_id_fkey';
-            columns: ['id'];
-            referencedRelation: 'users';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
-      profiles: {
+            foreignKeyName: "organizations_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      private_org_info: {
         Row: {
-          avatar_url: string | null;
-          full_name: string | null;
-          id: string;
-          updated_at: string | null;
-          username: string | null;
-          website: string | null;
-        };
+          id: string
+          idk_something_private: string | null
+        }
         Insert: {
-          avatar_url?: string | null;
-          full_name?: string | null;
-          id: string;
-          updated_at?: string | null;
-          username?: string | null;
-          website?: string | null;
-        };
+          id: string
+          idk_something_private?: string | null
+        }
         Update: {
-          avatar_url?: string | null;
-          full_name?: string | null;
-          id?: string;
-          updated_at?: string | null;
-          username?: string | null;
-          website?: string | null;
-        };
+          id?: string
+          idk_something_private?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: 'profiles_id_fkey';
-            columns: ['id'];
-            referencedRelation: 'users';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
+            foreignKeyName: "private_org_info_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      private_user_info: {
+        Row: {
+          first_name: string
+          id: string
+          identification: string | null
+          last_name: string
+        }
+        Insert: {
+          first_name: string
+          id: string
+          identification?: string | null
+          last_name: string
+        }
+        Update: {
+          first_name?: string
+          id?: string
+          identification?: string | null
+          last_name?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "private_user_info_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      regions: {
+        Row: {
+          alpha_2: string
+          alpha_3: string
+          country_code: string
+          intermediate_region: string | null
+          intermediate_region_code: string | null
+          iso_3166_2: string
+          name: string
+          region: string
+          region_code: string | null
+          sub_region: string | null
+          sub_region_code: string | null
+        }
+        Insert: {
+          alpha_2: string
+          alpha_3: string
+          country_code: string
+          intermediate_region?: string | null
+          intermediate_region_code?: string | null
+          iso_3166_2: string
+          name: string
+          region: string
+          region_code?: string | null
+          sub_region?: string | null
+          sub_region_code?: string | null
+        }
+        Update: {
+          alpha_2?: string
+          alpha_3?: string
+          country_code?: string
+          intermediate_region?: string | null
+          intermediate_region_code?: string | null
+          iso_3166_2?: string
+          name?: string
+          region?: string
+          region_code?: string | null
+          sub_region?: string | null
+          sub_region_code?: string | null
+        }
+        Relationships: []
+      }
       species: {
         Row: {
-          id: number;
-          name: string | null;
-        };
+          id: number
+          name: string | null
+        }
         Insert: {
-          id?: number;
-          name?: string | null;
-        };
+          id?: number
+          name?: string | null
+        }
         Update: {
-          id?: number;
-          name?: string | null;
-        };
-        Relationships: [];
-      };
+          id?: number
+          name?: string | null
+        }
+        Relationships: []
+      }
       users: {
         Row: {
-          first_name: string;
-          id: string;
-          identification: string;
-          last_name: string;
-          reputation: number | null;
-          username: string;
-        };
+          id: string
+          region: string | null
+          reputation: number | null
+          username: string
+        }
         Insert: {
-          first_name: string;
-          id: string;
-          identification: string;
-          last_name: string;
-          reputation?: number | null;
-          username: string;
-        };
+          id: string
+          region?: string | null
+          reputation?: number | null
+          username: string
+        }
         Update: {
-          first_name?: string;
-          id?: string;
-          identification?: string;
-          last_name?: string;
-          reputation?: number | null;
-          username?: string;
-        };
+          id?: string
+          region?: string | null
+          reputation?: number | null
+          username?: string
+        }
         Relationships: [
           {
-            foreignKeyName: 'users_id_fkey';
-            columns: ['id'];
-            referencedRelation: 'users';
-            referencedColumns: ['id'];
+            foreignKeyName: "users_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           },
-        ];
-      };
-    };
+          {
+            foreignKeyName: "users_region_fkey"
+            columns: ["region"]
+            referencedRelation: "regions"
+            referencedColumns: ["alpha_3"]
+          }
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
