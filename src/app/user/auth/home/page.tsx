cfd288d27849ec90e70d28c388abe681378b343a
sant9s <sgalarraga@itba.edu.ar>
2025-02-09 20:16:08 -0300

    start of a new dashboard

--- src/app/user/auth/home/page.tsx
+++ src/app/user/auth/home/page.tsx
@@ -7,6 +7,7 @@ import supabase from '~/supabase/helpers';
 import AnimalCard from '@/components/animalCard';
 import DonationHistory from '@/components/donations';
 import NotificationsHistory from '@/components/notifications';
+import { Text } from "@chakra-ui/react";
 import {
   Box,
   Flex,
@@ -42,12 +43,13 @@ import AdminSidebarResponsive from '@/components/adminSidebar';
       <AnimalCard key={animal.id} {...animal} />))}
 */
 
+
 export default function UserDashboard() {
   const bgCard = useColorModeValue('white', 'gray.700');
   return (
     <Flex p={8} flexDirection="column" justifyContent="center">
       <HStack>
-        <Flex mt={{ lg: '-50%', xl: '-30%' }}>
+        <Flex>
           <AdminSidebarResponsive />
         </Flex>
         <Flex
@@ -55,60 +57,25 @@ export default function UserDashboard() {
           w="100%"
           mb={8}
           px={{ base: 4, md: 8 }}
-          pt={{ base: '120px', md: '14%', lg: '12%' }}
+          pt={{ base: '100px', md: '12%', lg: '10%' }}
         >
-          <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing="24px">
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Hola
-            </Box>
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Hola
-            </Box>
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Hola
-            </Box>
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Hola
-            </Box>
-          </SimpleGrid>
-          <Grid
-            templateColumns={{ md: '1fr', lg: '1.8fr 1.2fr' }}
-            templateRows={{ md: '1fr auto', lg: '1fr' }}
-            my="26px"
-            gap="24px"
-          >
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Hola
-            </Box>
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Mundo
-            </Box>
-          </Grid>
-          <Grid
-            templateColumns={{ sm: '1fr', lg: '1.3fr 1.7fr' }}
-            templateRows={{ sm: 'repeat(2, 1fr)', lg: '1fr' }}
-            gap="24px"
-            mb={{ lg: '26px' }}
-          >
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Hola
-            </Box>
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Mundo
-            </Box>
-          </Grid>
-          <Grid
-            templateColumns={{ sm: '1fr', md: '1fr 1fr', lg: '2fr 1fr' }}
-            templateRows={{ sm: '1fr auto', md: '1fr', lg: '1fr' }}
-            gap="24px"
-          >
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Hola
-            </Box>
-            <Box h={200} borderRadius="15px" p={2} bg={bgCard}>
-              Mundo
-            </Box>
-          </Grid>
+          <Text fontSize="4xl" fontWeight="bold" mb={4}>Dashboard</Text>
+          
+          <Box mb={6} p={4} borderRadius="15px" bg={bgCard} boxShadow="md">
+            Solicitud 1
+          </Box>
+          
+          <Box mb={6} p={4} borderRadius="15px" bg={bgCard} boxShadow="md">
+          Solicitud 2
+          </Box>
+          
+          <Box mb={6} p={4} borderRadius="15px" bg={bgCard} boxShadow="md">
+            Solicitud 3
+          </Box>
+          
+          <Box mb={6} p={4} borderRadius="15px" bg={bgCard} boxShadow="md">
+          Solicitud 4
+          </Box>
         </Flex>
       </HStack>
     </Flex>
